e6a0be18c01c89e7a575e02fd87f41054bae1dff
Oladele <oladele2alo@gmail.com>
2022-05-11 07:40:38 +0200

    updateA

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import authRoute from "./routes/auth.js"
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
 
 
 const app = express();
@@ -20,7 +23,10 @@ mongoose.connection.on("disconnected", ()=>{
 })
 
 // middlewares
-app.use("/auth",authRoute)
+app.use("/api/v1/auth",authRoute);
+app.use("/api/v1/users",usersRoute);
+app.use("/api/v1/hotels",hotelsRoute);
+app.use("/api/v1/rooms",roomsRoute);
 
 app.listen(8800, ()=>{
     connect()
