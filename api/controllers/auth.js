fa6c7fd5037a2444b963ad1dfb2e42cc4b17fdd6
Oladele <oladele2alo@gmail.com>
2022-05-25 05:50:06 +0200

    commit

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,44 +1,44 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import { createError } from "../utils/error.js";
 
 
 export const register = async (req,res,next)=>  {
 
     try{
         
-        const salt = bcrypt.genSaltSync(10);
-        //const hashedPassword =  bcrypt.hashSync(req.body.password,salt);
-        const password = req.body.password;
+        const salt =  bcrypt.genSaltSync(10);
+        const hash =  bcrypt.hashSync(req.body.password,salt);
+       // const password = req.body.password;
 
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: password,
+            password: hash,
 
         })
         await newUser.save()
         res.status(200).send("User has been successfully created.")
 
     } catch(err) {
-        next(err)
+        next(err);
     }
 
 };
-
-
 export const login = async (req,res,next)=>  {
 
     try{
-        const user = User.findOne({username: req.body.username})
+        const user = await User.findOne({username: req.body.username})
         if(!user) return next(createError(404,"User not found!"))
         
-        const isPasswordCorrect = await bcrypt.compare(req.body.password,user.password)
-        if(!isPasswordCorrect) return next(createError(400,"Wrong password or username!"))
-        res.status(200).json(user)
-        
-
+        const isPasswordCorrect = await bcrypt.compare(
+            req.body.password,user.password);
+        if(!isPasswordCorrect)
+        return next(createError(400,"Wrong password or username!"));
+        res.status(200).json(user);        
     } catch(err) {
-        next(err)
+        next(err);
     }
 
 };
+
